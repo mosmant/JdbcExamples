9f832ee2c27c6411fa0a0299ec53cffe18592b43
mosmant <mottnr@gmail.com>
2021-09-15 23:13:53 +0300

    15.09.2021

---
+++ src/main/java/mOsmanTurker/JdbcExamples/Urun.java
@@ -0,0 +1,57 @@
+package mOsmanTurker.JdbcExamples;
+
+public class Urun {
+	
+	// POJO class ( plain old java object)
+	
+	private int id;
+	private String isim;
+	private double fiyat;
+	
+	
+	public Urun(int id, String isim, double fiyat) {
+		super();
+		this.id = id;
+		this.isim = isim;
+		this.fiyat = fiyat;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getIsim() {
+		return isim;
+	}
+
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+
+
+	public double getFiyat() {
+		return fiyat;
+	}
+
+
+	public void setFiyat(double fiyat) {
+		this.fiyat = fiyat;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Urun [id=" + id + ", isim=" + isim + ", fiyat=" + fiyat + "]";
+	}
+	
+	
+
+}
